089413bb4c794720110f1da561f69026b215b7da
SaadKhan6969 <ayazsaad57@gmail.com>
2023-12-17 17:21:07 +0500

    Work Accomplish

---
+++ class2.dart
@@ -0,0 +1,42 @@
+void main(){
+//   List se=[
+// 40,50,60
+//   ];
+//   List mylist =[
+//     1,
+//     2,
+//     3,
+//     "sad",
+//     "ham",
+//     'ada',
+
+//   ];
+//   // print(mylist[1]);
+  // // print(mylist.length);
+  // mylist.replaceRange((mylist.length -4);(mylist.length -1);[1,2,3,4] );
+  // // mylist.sort();
+  // // print(mylist.isEmpty);
+  // print(mylist);
+  // mylist.insert(2,40);
+  // mylist.add(40);
+  // mylist.insertAll(5,[87,76,65,54,43]);
+  // mylist.remove(1);
+  // mylist.clear();
+  // print(mylist);
+
+  // Map mymap = {
+  // "name" : "Saad Khan",
+  // "contact-info" : {
+  //   "email" : "sad123@gmail.com",
+  //   "phone": "0306000",
+  // }
+  // };
+  // // print(mymap.isNotEmpty);
+  // // print(mymap['contact-info']['phone']);
+  // // mymap["name"]="Karachi pakistan";
+  // mymap.putIfAbsent("name",()=> 'karachi pakistan');
+  // print(mymap);
+
+
+
+}
